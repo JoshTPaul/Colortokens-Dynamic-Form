134b046a6ee4aeffb3fce62b11c876591a7cb8a4
Joshua Paul <joshpauljtp@gmail.com>
2024-05-02 00:43:04 +0530

    CSS cleanup + basic styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,9 @@
-import "./App.css";
-import reactLogo from "./assets/react.svg";
 import FormGenerator from "./components/Form";
-import viteLogo from "/vite.svg";
 
 function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      <h1>Colortokens Take Home Assignment</h1>
       <FormGenerator />
     </>
   );
