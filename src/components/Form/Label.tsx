b73f56e43f81e27fe9ae8044c4a5cba43bb718d2
Joshua Paul <joshpauljtp@gmail.com>
2024-05-02 01:15:14 +0530

    Required field indicator

--- src/components/Form/Label.tsx
+++ src/components/Form/Label.tsx
@@ -1,12 +1,18 @@
 import { FormElement } from "../../types";
 import Tooltip from "./Tooltip";
 
-type Props = Pick<FormElement, "displayName" | "information" | "key">;
+type Props = Pick<
+  FormElement,
+  "displayName" | "information" | "key" | "required"
+>;
 
-function Label({ key: htmlFor, displayName, information }: Props) {
+function Label({ key: htmlFor, displayName, information, required }: Props) {
   return (
     <label htmlFor={htmlFor}>
-      {displayName}
+      <span>
+        {displayName}
+        {required && <sup>*</sup>}
+      </span>
       <Tooltip text={information} />
     </label>
   );
